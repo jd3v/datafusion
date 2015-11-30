fd14ffd0486e56a62b9cf2f225e81de69a187f0d
Julian <julian.jo.otto@gmail.com>
2015-11-30 16:03:24 +0100

    - joe liebt daniel

--- src/main/java/de/uni_mannheim/informatik/wdi/datafusion/usecase/wdiproject/evaluation/NameEvaluationRule.java
+++ src/main/java/de/uni_mannheim/informatik/wdi/datafusion/usecase/wdiproject/evaluation/NameEvaluationRule.java
@@ -1,7 +1,7 @@
 package de.uni_mannheim.informatik.wdi.datafusion.usecase.wdiproject.evaluation;
 
 import de.uni_mannheim.informatik.wdi.datafusion.evaluation.EvaluationRule;
-import de.uni_mannheim.informatik.wdi.datafusion.usecase.movies.FusableCity;
+import de.uni_mannheim.informatik.wdi.datafusion.usecase.wdiproject.FusableCity;
 import de.uni_mannheim.informatik.wdi.identityresolution.similarity.SimilarityMeasure;
 import de.uni_mannheim.informatik.wdi.identityresolution.similarity.string.TokenizingJaccardSimilarity;
 
@@ -12,7 +12,7 @@ public class NameEvaluationRule extends EvaluationRule<FusableCity> {
 	@Override
 	public boolean isEqual(FusableCity record1, FusableCity record2) {
 		// the title is correct if all tokens are there, but the order does not matter
-		return sim.calculate(record1.getTitle(), record2.getTitle()) == 1.0;
+		return sim.calculate(record1.getName(), record2.getName()) == 1.0;
 	}
 
 }
